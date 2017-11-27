5bb93b2e1c1afcf0c3d8d1e28d7128a7c7954f2c
Chillaruna <mglennschmitz@gmail.com>
2017-11-27 16:05:28 +0100

    Trying to get it work

---
+++ Lab1/Labb1.py
@@ -0,0 +1,17 @@
+#Skriv en egen klass som representerar en plats.
+#Klassen ska ha attribut för alla data i filen.
+#Klassen ska ha metoderna __str__ och __init__
+#Du kan om du vill även ha en till klass som håller reda på platserna.
+#Utöver __str__ och __init__ ska du skriva minst tre metoder (medlemsfunktioner) sammanlagt (antingen i platsklassen eller den valfria klassen för alla platser).
+
+#Skriv en funktion som läser in data från filen, skapar objekt, och lagrar objekten i en lista. (lista = []).
+#Skriv ett program, uppdelat i lämpliga funktioner (eller metoder/medlemsfunktioner), där man kan söka i listan.
+#Vid redovisning ska du kunna förklara hur klasser/objekt/listor/filhantering fungerar i Python.
+#Betyg
+#betyg E:
+#Ditt program löser uppgiften
+#Ditt program uppfyller kraven för ett perfekt program
+#Du kan svara tillfredsställande på frågor om labben
+
+
+print ("Hello World")
