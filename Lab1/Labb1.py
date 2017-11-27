a0ae3755f54dc3967dd05bdb0ba469a937cc9e86
Chillaruna <mglennschmitz@gmail.com>
2017-11-27 16:29:22 +0100

    started the labb
    added methods str and init

--- Lab1/Labb1.py
+++ Lab1/Labb1.py
@@ -3,7 +3,6 @@
 #Klassen ska ha metoderna __str__ och __init__
 #Du kan om du vill även ha en till klass som håller reda på platserna.
 #Utöver __str__ och __init__ ska du skriva minst tre metoder (medlemsfunktioner) sammanlagt (antingen i platsklassen eller den valfria klassen för alla platser).
-
 #Skriv en funktion som läser in data från filen, skapar objekt, och lagrar objekten i en lista. (lista = []).
 #Skriv ett program, uppdelat i lämpliga funktioner (eller metoder/medlemsfunktioner), där man kan söka i listan.
 #Vid redovisning ska du kunna förklara hur klasser/objekt/listor/filhantering fungerar i Python.
@@ -15,3 +14,17 @@
 
 
 print ("Hello World")
+
+class Location(): #skriv en egen klass som representerar en plats
+#klassen ska ha metoden __str__
+    def __str__(self):
+        return self.namn 
+
+#klassen ska ha metoden __init__
+    def __init__(self,namn,beskr,lat,long,datum):
+        self.name = name
+        self.beskr = beskr
+        self.lat = lat
+        self.long = long
+        self.datum = datum
+#
